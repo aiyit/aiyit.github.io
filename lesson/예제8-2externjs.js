170979a358becf77ba92ed5198420c0a9d228370
AI 소프트웨어과 <geniousfather@gmail.com>
2025-06-06 12:09:31 +0900

    Add files via upload

---
+++ lesson/예제8-2externjs.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>예제8-2외부 자바스크립트</title>
+	<script src="js/예제8-1jscode.js"></script>
+</head>
+<body>
+	<h1>AI JavaScript Page</h1>
+</body>
+</html>
+
