3d1876773eb62a3e4d739ab83199a8973970be8d
aiyit <geniousfather@gmail.com>
2026-09-15 14:33:21 +0900

    Fix script source path in external JS example

--- lesson/예제8-2externjs.js
+++ lesson/예제8-2externjs.js
@@ -2,7 +2,7 @@
 <html>
 <head>
         <title>예제8-2외부 자바스크립트</title>
-	<script src="js/예제8-1jscode.js"></script>
+	<script src="예제8-1jscode.js"></script>
 </head>
 <body>
 	<h1>AI JavaScript Page</h1>
