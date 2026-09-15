68867b6b1df0ec8b69a3094edcf392bec1343976
aiyit <geniousfather@gmail.com>
2026-09-15 14:34:40 +0900

    Replace HTML with JavaScript output
    Removed HTML structure and added JavaScript output.

--- lesson/예제8-2externjs.js
+++ lesson/예제8-2externjs.js
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <title>예제8-2외부 자바스크립트</title>
-	<script src="예제8-1jscode.js"></script>
-</head>
-<body>
-	<h1>AI JavaScript Page</h1>
-</body>
-</html>
-
+document.write("My first Javascript Using AI");
